6eab3aebb4b439bbcc3b52f1efbdaef3ae9528a5
seanhwang78-png <seanhwang78@gmail.com>
2026-05-24 13:09:21 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const SHEET_ID = "1i5ssylMyIHv-lMr38y_jk4jP0j--DqI4bG2ge1MgIWQ";
 
